9167634157310d4aa5b43c804a67a3eaa26fa9ed
root <root@ip-172-31-47-140.us-east-2.compute.internal>
2018-10-29 03:50:44 +0000

    created new repo

---
+++ src/main/java/com/infosys/App.java
@@ -0,0 +1,13 @@
+package com.infosys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
